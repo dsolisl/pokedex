ded002fbbf7084fb35122d30c5521342904a5754
Diana Solis <d.solis.l@hotmail.com>
2018-06-06 18:56:00 -0700

    primera version de pokedex

---
+++ pokedex/action.js
@@ -0,0 +1,99 @@
+// var pokemones = [
+//   {
+//     "id":"001",
+//     "name":"Charmander",
+//     "photo":"01",
+//     "type":"fire"
+//   },
+//   {
+//     "id":"002",
+//     "name":"Kanjaskan",
+//     "photo":"02",
+//     "type":"fighting"
+//   },
+//   {
+//     "id":"003",
+//     "name":"Mewtwo",
+//     "photo":"03",
+//     "type":"fairy"
+//   },
+//   {
+//     "id":"004",
+//     "name":"Blastoise",
+//     "photo":"04",
+//     "type":"water"
+//   },
+//   {
+//     "id":"005",
+//     "name":"Bulbasaur",
+//     "photo":"05",
+//     "type":"grass"
+//   },
+//   {
+//     "id":"006",
+//     "name":"Magikarp",
+//     "photo":"06",
+//     "type":"water"
+//   },
+//   {
+//     "id":"007",
+//     "name":"Pikachu",
+//     "photo":"07",
+//     "type":"electric"
+//   },
+//   {
+//     "id":"008",
+//     "name":"Gengar",
+//     "photo":"08",
+//     "type":"ghost"
+//   },
+//   {
+//     "id":"009",
+//     "name":"Vamoacalmarno",
+//     "photo":"09",
+//     "type":"water"
+//   }
+// ]; 
+
+function json(){
+}
+   
+      
+      function showPokemon (query){
+          var result = "";
+          if (query == null){
+            console.log ("show all");
+            for (i=0; i<pokemones.length; i++){
+              result += pokemones[i].name + "<br>";
+            }
+            document.getElementById("result").innerHTML = result;
+          }
+          else {
+            console.log (query);
+            for (var i=0; i<pokemones.length; i++){
+              if(pokemones[i].name.toLowerCase().search(query) >=0){
+                result += pokemones[i].name + "<br>";
+              }
+            }
+            document.getElementById("result").innerHTML = result;
+          }
+      }
+       function searchPokemon(){
+         var query = document.getElementById("query").value;
+         
+         showPokemon(query);
+       }
+      var content = "";
+
+      for (var i=0; i<pokemones.length; i++){
+          content += "<div class = 'pokeFriends'>";
+          content += "<img  class= 'bicho' src = 'img/photos/" + pokemones[i].photo +'.jpg'+ "'/>";
+          content += "<h2 class= 'nombre'>" + pokemones[i].name + "</h2>";
+          content += "<div class = 'numnombre'>";
+          content += "<h1 class= 'numero'>" + pokemones[i].id + "</h1>";
+          content += "<img class='tipo' src = 'img/types/" + pokemones[i].type +'.png' +  "'/>";
+          content += "</div>";
+          content += "</div>";
+ 
+      }
+        document.getElementById("result").innerHTML = content;
